fb3504d70152bac591f51183172c31a27097df0d
mickmed <mick2090@gmail.com>
2019-11-26 15:45:42 -0500

    style updates

--- src/js/projects.js
+++ src/js/projects.js
@@ -34,96 +34,53 @@ export let projects = async (type, data = []) => {
       // console.log(projWrapper)
       projWrapper.style.width = '110%'
       projWrapper.style.fontSize = '2em'
-
-
     }
 
     let project = makeElement('div', 'project', projWrapper)
     project.classList.add('project' + e.id, 'projectCard')
-    project.addEventListener('click', async function (evt) {
-      let res = await showSingleItemm(evt, e, 'projects')
+    // project.addEventListener('click', async function (evt) {
+    //   let res = await showSingleItemm(evt, e, 'projects')
+
+    //   document.querySelector('.main-content').innerHTML = ''
+    //   navbar({ project: e })
+    //   projects('project', { 'project': e, 'technologies': res.technologies, 'traits': res.traits })
+    //   technologies('technologies', { 'technologies': res.technologies })
+    // })    // let anchor = makeElement('a', 'proj-link', project, { href: e.site_url, target: "_blank" })
+
+    let imgWrapper = makeElement('div', 'img-wrapper', project)
+    let img = makeElement('img', 'img', imgWrapper, { src: 'src/img/' + (e.local_url) })
+   
+
+    let modal = makeElement('div', 'modal', imgWrapper, {})
+   
+    modal.classList.add('project-' + e.id + '-modal', 'modal-slide')
+    let modalSpan = makeElement('span', 'modal-span', modal)
+    let modalh2 = makeElement('h2', 'project-title', modalSpan, { innerText: e.title })
+
+    let linkModal = makeElement('div', 'link-modal', imgWrapper)
+    let linkModalMore = makeElement('p', 'more-anchor', linkModal, { innerText:'...more' })
+    linkModalMore.addEventListener('click', async(evt)=>{
+        let res = await showSingleItemm(evt, e, 'projects')
 
       document.querySelector('.main-content').innerHTML = ''
       navbar({ project: e })
       projects('project', { 'project': e, 'technologies': res.technologies, 'traits': res.traits })
       technologies('technologies', { 'technologies': res.technologies })
-    })    // let anchor = makeElement('a', 'proj-link', project, { href: e.site_url, target: "_blank" })
-
-    let imgWrapper = makeElement('div', 'img-wrapper', project)
-    // console.log(e.local_url)
-    let img = makeElement('img', 'img', imgWrapper, { src: 'src/img/' + (e.local_url) })
+    })
 
 
-    let modal = makeElement('div', 'modal', imgWrapper, {})
-    modal.classList.add('project-' + e.id + '-modal', 'modal-slide')
-    let modalSpan = makeElement('span', 'modalSpan', modal)
+    let linkModalSiteBtn = makeElement('button', 'site-button', linkModal, { innerText: 'visit site'})
+    linkModalSiteBtn.addEventListener('click', (evt)=> {
+      // evt.stopPropagation()
+      console.log('hid')
+      location.href = e.site_url
+      target = "_blank"
+     
+    })
+   
 
-    console.log(e.target)
-    let hoverModal = makeElement('div', 'hover-modal', imgWrapper)
-    // hoverModal.style.opacity = 0
-    let hoverModalSpan = makeElement('span', 'hover-modal-span', hoverModal)
-    // hoverModalSpan.style.display = "none"
 
-    let modalh2 = makeElement('h2', 'project-title', modal, { innerText: e.title })
-    let hoverModalh2 = makeElement('h2', 'project-title', hoverModalSpan, { innerText: e.title })
 
-    let hoverModalButton = makeElement('button', 'site-button', hoverModalSpan, { innerText: 'open' })
-    hoverModalButton.style.display = "none"
-
-//     modal.addEventListener('mouseenter', (evt) => {
-//       evt.stopPropagation()
-//       console.log('over', evt.target)
-//       let init = 0
-//       let t = setInterval(()=>{
-//         // console.log('init', init)
-//         if(init >= 100){
-//           clearInterval(t)
-         
-//         }else{
-//           // console.log('init', 1/init)
-//           // modalSpan.style.opacity = 1 - (.01 * init)
-//           modalSpan.style.left = init/100 * -60 + '%'
-//           init ++
-//         }
-//       },1)
-//       modalh2.style.display = "none"
-//       // modal.style.display = "none"
-
-//       hoverModal.style.display = 'flex'
-//       hoverModalh2.style.display = "block"
-//       hoverModalButton.style.display = "block"
-
-//       hoverModalSpan.style.display = "flex"
-      
-//     },true)
-// hoverModalSpan.addEventListener('mouseleave', (event) => {
-  
-//       event.stopPropagation()
-//       console.log('out', event.target)
-//       console.log(event.relativeTarget)
-
-//         modal.style.display = "flex"
-//         modalSpan.style.display = "flex"
-//         modalSpan.style.opacity = 1
-//         modalSpan.style.left = 0
-
-//         hoverModal.style.display = 'none'
-//         hoverModalSpan.style.display = "none"
-//         modalh2.style.display = "inline"
-//       },true)
-      // modal.addEventListener('mouseout', (event) => {
-      //   event.stopPropagation()
-      //   console.log('out', event.target)
-  
-      //     modal.style.display = "flex"
-      //     modalSpan.style.display = "flex"
-      //     modalSpan.style.opacity = 1
-      //     modalSpan.style.left = 0
-  
-      //     hoverModal.style.display = 'none'
-      //     hoverModalSpan.style.display = "none"
-      //     modalh2.style.display = "inline"
-      //   },true)
 
     let h3
     console.log('res.traits', res.traits)
@@ -133,8 +90,9 @@ export let projects = async (type, data = []) => {
 
         h3 = makeElement('h3', 'trait', traitsDiv, { innerText: trait.name })
       })
-
-
+      
+      
+  
 
     if (type === 'project' && window.matchMedia("(min-width: 600px)").matches) {
       modal.style.backgroundImage = 'linear-gradient(to right, rgba(255,255,255, 1),rgba(255, 255, 255, 1) )'
